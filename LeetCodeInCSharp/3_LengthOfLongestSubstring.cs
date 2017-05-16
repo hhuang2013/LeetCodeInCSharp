2a8a068a7f7744e60d9f8144370479f526f9b67c
hhuang2013 <herohest@163.com>
2017-05-16 17:34:19 +0800

    add #3

---
+++ LeetCodeInCSharp/3_LengthOfLongestSubstring.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class LengthOfLongestSubstringSolution
+    {
+        //public static int LengthOfLongestSubstring(string s)
+        //{
+        //    if (s.Length <= 1)
+        //        return s.Length;
+
+        //    int maxlen = 1;
+        //    for (int i = 0; i < s.Length - 1; i++)
+        //    {
+        //        int len = 1;
+        //        for (int j = i + 1; j < s.Length; j++)
+        //        {
+        //            string sub = s.Substring(i, j - i);
+        //            if (sub.Contains(s[j]))
+        //                break;
+        //            len++;
+        //        }
+        //        if (len > maxlen)
+        //            maxlen = len;
+        //    }
+        //    return maxlen;
+        //}
+
+        //public static int LengthOfLongestSubstring(string s)
+        //{
+        //    if (s.Length <= 1)
+        //        return s.Length;
+
+        //    int maxlen = 1;
+        //    StringBuilder subbd =new StringBuilder();
+        //    for (int i = 0; i < s.Length - 1; i++)
+        //    {
+        //        int len = 1;
+        //        subbd.Clear(); 
+        //        for (int j = i + 1; j < s.Length; j++)
+        //        {
+        //            subbd.Append(s[j-1]);
+        //            string sub = subbd.ToString();
+        //            if (sub.Contains(s[j]))
+        //                break;
+        //            len++;
+        //        }
+        //        if (len > maxlen)
+        //            maxlen = len;
+        //    }
+        //    return maxlen;
+        //}
+
+
+        public static int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length <= 1)
+                return s.Length;
+
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            int maxlen = 0;
+
+            for (int i = 0, j = 0; i < s.Length; i++)
+            {
+                if(map.ContainsKey(s[i]))
+                {
+                    j = Math.Max(j,map[s[i]])+1;
+                    map[s[i]] = i;
+                }
+                else
+                {
+                    map.Add(s[i], i);
+                }
+                maxlen = Math.Max(maxlen, i - j + 1);   
+            }
+            return maxlen;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest3_LengthOfLongestSubstring.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest3_LengthOfLongestSubstring
+    {
+        [TestMethod]
+        public void TestMethodLengthOfLongestSubstring()
+        {
+            Assert.AreEqual(3,LengthOfLongestSubstringSolution.LengthOfLongestSubstring("abcabcbb"));
+            Assert.AreEqual(1, LengthOfLongestSubstringSolution.LengthOfLongestSubstring("bbbbb"));
+            Assert.AreEqual(3, LengthOfLongestSubstringSolution.LengthOfLongestSubstring("pwwkew"));
+            Assert.AreEqual(95, LengthOfLongestSubstringSolution.LengthOfLongestSubstring("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ "));
+            Assert.AreEqual(2, LengthOfLongestSubstringSolution.LengthOfLongestSubstring("abba"));
+            Assert.AreEqual(5, LengthOfLongestSubstringSolution.LengthOfLongestSubstring("tmmzuxt"));
+         }
+    }
+}
