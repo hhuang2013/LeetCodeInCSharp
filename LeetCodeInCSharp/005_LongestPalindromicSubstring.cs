a1e622097a278d61b6ccff75d563cc18a85eab78
hhuang2013 <herohest@163.com>
2017-05-17 17:37:19 +0800

    add #5

--- LeetCodeInCSharp/005_LongestPalindromicSubstring.cs
+++ LeetCodeInCSharp/005_LongestPalindromicSubstring.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace LeetCodeInCSharp
 {
-    class LongestPalindromicSubstring
+    public class LongestPalindromicSubstringSolution
     {
+        public static string LongestPalindrome(string s)
+        {
+            if (s.Length <= 1)
+                return s;
+
+            string ans="";
+            int maxlen = 0;
+            for (int i = 0; i < s.Length;i++)
+            {
+                for (int j = i,len=0; j < s.Length; j++)
+                {
+                    len = j - i + 1;
+                    if (len <= maxlen)
+                        continue;
+                    string sub = s.Substring(i, len);
+
+                    if (isPalindromic(sub))
+                    {
+                        ans = sub;
+                        maxlen = sub.Length;
+                    }
+                }
+            }
+            return ans;
+        }
+        public static bool isPalindromic(string s)
+        {
+            for(int i=0;i<s.Length/2;i++)
+            {
+                if (s[i] != s[s.Length-1-i])
+                    return false;
+            }
+            return true;
+        }
     }
 }
---
+++ LeetCodeInCSharpUnitTest/UnitTest5_LongestPalindromicSubstring.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest5_LongestPalindromicSubstring
+    {
+        [TestMethod]
+        public void T005__LongestPalindromicSubstring()
+        {
+            Assert.AreEqual("bab", LongestPalindromicSubstringSolution.LongestPalindrome("babad"));
+            Assert.AreEqual("bb", LongestPalindromicSubstringSolution.LongestPalindrome("cbbd"));
+            Assert.AreEqual("bbbbbbbb", LongestPalindromicSubstringSolution.LongestPalindrome("bbbbbbbb"));
+            Assert.AreEqual("a", LongestPalindromicSubstringSolution.LongestPalindrome("abcda"));
+            Assert.AreEqual("a", LongestPalindromicSubstringSolution.LongestPalindrome("a"));
+        }
+    }
+}
